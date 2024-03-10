e7d38eadafd04b586fae23a8ddeed0ede3e518b9
Kevinsorensen523 <94046712+Kevinsorensen523@users.noreply.github.com>
2024-03-10 19:38:20 +0700

    Add StorePage

--- GarudaFood_Profile/src/pages/StorePage.tsx
+++ GarudaFood_Profile/src/pages/StorePage.tsx
@@ -1,22 +1,52 @@
 import React from "react";
-import { IonContent, IonHeader, IonTitle, IonToolbar } from "@ionic/react";
+import {
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonThumbnail,
+} from "@ionic/react";
 import "./../styles.css";
 
-const StorePage = () => (
-  <>
+const StorePage = () => {
+  const imageUrls = [
+    "https://ionicframework.com/docs/img/demos/thumbnail.svg",
+    "https://ionicframework.com/docs/img/demos/thumbnail.svg",
+    "https://ionicframework.com/docs/img/demos/thumbnail.svg",
+    "https://ionicframework.com/docs/img/demos/thumbnail.svg",
+  ];
+
+  const Harga = ["Rp 100.000", "Rp 150.000", "Rp 200.000", "Rp 250.000"];
+
+  return (
     <IonContent>
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          height: "100%",
-        }}
-      >
-        News Contenct
-      </div>
+      <IonGrid className="mt-20">
+        <IonRow>
+          {imageUrls.map((imageUrl, index) => (
+            <IonCol size="6" key={index}>
+              <IonCard className="">
+                <IonThumbnail className="mx-auto items-center w-auto h-96 my-4">
+                  <img
+                    src={imageUrl}
+                    alt={`Image ${index + 1}`}
+                    className="w-auto h-96 mx-auto"
+                  />
+                </IonThumbnail>
+                <IonCardHeader className="text-center">
+                  <IonCardTitle className="text-lg">
+                    {Harga[index]}
+                  </IonCardTitle>
+                </IonCardHeader>
+              </IonCard>
+            </IonCol>
+          ))}
+        </IonRow>
+      </IonGrid>
     </IonContent>
-  </>
-);
+  );
+};
 
 export default StorePage;
